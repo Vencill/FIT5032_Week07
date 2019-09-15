3e1e50826c27cba093117e4d26a245de5ad5d606
Vencill <wliu0028@student.monash.edu>
2019-09-15 14:10:10 +1000

    Add project files.

---
+++ SQLQuery1.sql
@@ -0,0 +1,20 @@
+﻿CREATE TABLE [dbo].[Students] (
+    [Id] int IDENTITY(1,1) NOT NULL,
+    [FirstName] nvarchar(max)  NOT NULL,
+    [LastName] nvarchar(max)  NOT NULL,
+	[UserId] nvarchar(max)  NOT NULL
+	primary key (Id)
+);
+GO
+
+-- Creating table 'UnitSet'
+CREATE TABLE [dbo].[Units] (
+    [Id] int IDENTITY(1,1) NOT NULL,
+    [Name] nvarchar(max)  NOT NULL,
+    [Description] nvarchar(max)  NOT NULL,
+    [StudentId] int  NOT NULL
+	primary key (Id),
+	foreign key (StudentId) references Students(Id)
+
+);
+GO
